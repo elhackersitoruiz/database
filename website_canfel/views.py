187f59e47703a4737f97fc68471593e1fb16259a
Joaquin <joaquingc123@gmail.com>
2024-09-24 19:17:37 -0500

    Add api endpoint

--- website_canfel/views.py
+++ website_canfel/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from website_canfel.models import Mascota
+from django.forms.models import model_to_dict
+from django.shortcuts import render, get_object_or_404
+
+
+def mascota_listing(request):
+    #mascota_list = [dict(m) for m in Mascota.objects.all()]
+    mascota_list = list(Mascota.objects.values())
+    return JsonResponse(mascota_list, safe=False)
+
+def mascota_by_id(request, id):
+    mascota =  model_to_dict(Mascota.objects.get(id=id))
+    return JsonResponse(mascota, safe=False)
+
 
 # Create your views here.
+
+
